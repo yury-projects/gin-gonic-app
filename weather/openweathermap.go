1a14d18361c73b8d8d4f4bce7cb53def23b4465f
yuribol <yuri.csf@gmail.com>
2017-12-24 22:44:01 -0500

    Cleaning up weather package, returning struct in current weather func

--- weather/openweathermap.go
+++ weather/openweathermap.go
@@ -1,20 +1,23 @@
 package weather
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"time"
 )
 
 const (
-	OPEN_WEATHER_MAP_API_KEY     = "2f83657530b7825a2abf5d2f9fa37b60"
 	BASE_API_URL                 = "api.openweathermap.org"
 	API_ENDPOINT_CURRENT_WEATHER = "https://" + BASE_API_URL + "/data/2.5/weather"
 	API_ENDPOINT_5DAY_FORECAST   = "https://" + BASE_API_URL + "/data/2.5/forecast"
 )
 
 var (
+	OPEN_WEATHER_MAP_API_KEY = os.Getenv("GIN_GONIC_OWM_API_KEY")
+
 	requestParams = map[string]string{
 		"units": "metric",
 		"APPID": OPEN_WEATHER_MAP_API_KEY,
@@ -25,26 +28,51 @@ var (
 	}
 )
 
+type main struct {
+	Temp     float32 `json:"temp"`
+	Pressure int     `json:"pressure"`
+	Humidity int     `json:"humidity"`
+	TempMin  float32 `json:"temp_min"`
+	TempMax  float32 `json:"temp_max"`
+}
+
+type weather struct {
+	Id          int    `json:"-"`
+	Title       string `json:"main"`
+	Description string `json:"description"`
+	Icon        string `json:"icon"`
+}
+
+type wind struct {
+	Speed float32 `json:"speed"`
+	Deg   float32 `json:"deg"`
+	Gust  float32 `json:"gust"`
+}
+
+type CurrentWeatherResponse struct {
+	Weather  []weather `json:"weather"`
+	Main     main      `json:"main"`
+	Wind     wind      `json:"wind"`
+	Datetime int       `json:"dt"`
+}
+
 func makeAPICall() {
 
 }
 
-func CurrentWeatherFromCity(city string) {
-	fmt.Println("Hello world")
+func CurrentWeatherFromCity(city string) *CurrentWeatherResponse {
 
 	req, _ := http.NewRequest("GET", API_ENDPOINT_CURRENT_WEATHER, nil)
 
 	query := req.URL.Query()
 
+	requestParams["q"] = city
+
 	for paramName, paramValue := range requestParams {
 		query.Add(paramName, paramValue)
 	}
 
-	query.Add("q", city)
-
 	req.URL.RawQuery = query.Encode()
-
-	fmt.Println(req.URL.String())
 
 	response, err := client.Do(req)
 
@@ -54,11 +82,23 @@ func CurrentWeatherFromCity(city string) {
 
 	body, readErr := ioutil.ReadAll(response.Body)
 
-	fmt.Println(readErr)
+	if readErr != nil {
+		fmt.Println(readErr)
+	}
+
+	defer response.Body.Close()
+
+	cwr := &CurrentWeatherResponse{}
 
 	fmt.Println(string(body))
 
-	defer response.Body.Close()
+	err = json.Unmarshal(body, cwr)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return cwr
 
 }
 
